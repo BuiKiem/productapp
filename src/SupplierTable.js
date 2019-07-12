dd0e881add8c38c26572f1902b94b8bf5ce335e4
Bui Kiem <buivantrung1105@gmail.com>
2019-07-12 20:53:26 +0700

    Create SupplierTable component.

---
+++ src/SupplierTable.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {SupplierTableRow} from "./SupplierTableRow";
+
+export const SupplierTable = (props) => {
+    return (
+        <table className="table table-sm table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>City</th>
+                <th>Products</th>
+                <th/>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                props.suppliers.map(s =>
+                    <SupplierTableRow
+                        supplier={s} key={s.id}
+                        editCallback={props.editCallback}
+                        deleteCallback={props.deleteCallback}
+                    />
+                )
+            }
+            </tbody>
+        </table>
+    );
+};
